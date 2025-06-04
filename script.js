eecb19bd5ac04e977488f6ac9f990b969e739c85
SD-Singh69 <sehajdeep2649@gmail.com>
2025-06-04 21:58:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,21 +18,13 @@ const gencompchoice = () => {
   return options[randidx];
 };
 
-
-
-const draw = () => {
+const Draw = () => {
   submit.innerText = "its a Draw! Want to play another round?";
   submit.style.backgroundColor = "#6e83d3";
 };
 
-
-
-
-
-
-
-const winner = (userwin) => {
-  if (userwin) {
+const winner = (UserWin) => {
+  if (UserWin) {
     userscore++;
     user.innerText = userscore;
     submit.innerText = "You won! Want to play another round?";
@@ -45,36 +37,27 @@ const winner = (userwin) => {
   }
 };
 
-
-
-
-
-
-const game = (userchoice) => {
+const game = (UserChoice) => {
   let compchoice = gencompchoice();
-  console.log("You chose:", userchoice);
+  console.log("You chose:", UserChoice);
   console.log("Computer chose:", compchoice);
-  if (userchoice == compchoice) {
-    return draw();
+  if (UserChoice == compchoice) {
+    return Draw();
   }
-  let userwin = true;
-  if (userchoice == "rock") {
-    userwin = compchoice == "paper" ? false : true;
-  } else if (userchoice == "paper") {
-    userwin = compchoice == "scissors" ? false : true;
-  } else if (userchoice == "scissors") {
-    userwin = compchoice == "rock" ? false : true;
+  let UserWin = true;
+  if (UserChoice == "rock") {
+    UserWin = compchoice == "paper" ? false : true;
+  } else if (UserChoice == "paper") {
+    UserWin = compchoice == "scissors" ? false : true;
+  } else if (UserChoice == "scissors") {
+    UserWin = compchoice == "rock" ? false : true;
   }
-  winner(userwin);
+  winner(UserWin);
 };
-
-
-
-
 
 items.forEach((item) => {
   item.addEventListener("click", () => {
-    const userchoice = item.getAttribute("id");
-    game(userchoice);
+    const UserChoice = item.getAttribute("id");
+    game(UserWin);
   });
 });
